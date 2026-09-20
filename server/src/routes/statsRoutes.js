2886dfe7dc6eab4da5ca7f5efde090a7d0c5d9dd
kunaal <skunaal57@gmail.com>
2026-09-20 13:51:49 +0530

    chore: clean workspace of legacy artifacts

--- server/src/routes/statsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDashboardStats } = require('../controllers/statsController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.use(protect);
-router.get('/', getDashboardStats);
-
-module.exports = router;
